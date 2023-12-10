444762223d56c8e0b4ed2c3384ec590d0ad5f761
Sumed Tamang <xumed001@gmail.com>
2023-12-10 16:58:05 -0600

    updated css styles added projects section

--- src/main.js
+++ src/main.js
@@ -3,5 +3,18 @@ import '../styles/style.css'
 import '../styles/components/header.css'
 import '../styles/components/hero.css'
 import '../styles/components/about.css'
+import '../styles/components/projects.css'
 import '../styles/utils.css'
 
+// project hover effect
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
