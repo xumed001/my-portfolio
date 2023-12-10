6a6abfba83bf390d7a97da98dfe268d169de2676
Sumed Tamang <xumed001@gmail.com>
2023-12-09 22:37:48 -0600

    CSS updates added more to hero section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import '../styles/morden-normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/about.css'
 import '../styles/utils.css'
 
